11c243e37e505bffdfbda85a87e1b7cadc17f884
Arthur Koehl <avkoehl@ucdavis.edu>
2021-07-19 00:36:56 -0700

    newspapers state column follows logic of issues state column

--- R/newspapers.R
+++ R/newspapers.R
@@ -13,7 +13,18 @@ get_newspapers = function() {
     # postprocess the newspapers
     # lccn is not unique in the newpspapers metadata
     # sometimes it has additional state values
+    # combine the newspapers's state assignments into a character vector
     newspapers = aggregate(data=newspapers, state ~ lccn + url + title, FUN=c)
+    newspapers$states = newspapers$state
+
+
+    # in keeping with the logic from issues
+    # that the last state is the most important one
+    # for example https://chroniclingamerica.loc.gov/lccn/sn83045160/
+    # notice that tennessee is the fourth state for that lccn in the newspapers
+    # dataframe and the last is the issues place column
+    newspapers$state = sapply(newspapers$states, tail, 1)
+
+    # also 
     tibble::as_tibble(newspapers)
 }
-
