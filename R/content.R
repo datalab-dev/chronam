1a49d82f8733ffee42ec4f1ccd9cf1cea5dbae6e
Arthur Koehl <avkoehl@ucdavis.edu>
2021-07-23 11:02:05 -0700

    using httr instead of RCurl

--- R/content.R
+++ R/content.R
@@ -21,7 +21,7 @@ get_batch_from_issue = function(issue_url) {
     # alternativley get list of lccns => search on batchesdf => list of batches
 }
 
-#' Download the batch from chronicling America
+#' Download the batch ocr data from chronicling America
 #' 
 #' bulk data url: http://chroniclingamerica.loc.gov/data/batches/<batch_name>
 #' bulk ocr data url: http://chroniclingamerica.loc.gov/data/ocr/<batch_name>.tar.bz2
@@ -32,34 +32,28 @@ get_batch_from_issue = function(issue_url) {
 #'
 #' the ocr data url seems be missing batches ingested in the current month
 #' @param batch_name character name of batch e.g wvu_els_ver01
-#' @param type character optional name of type of data you want to download for the\
-#' batch, one of c("data", "ocr")
 #' @param odir optional character path of directory to download the files into
-#' @importFrom Rcurl getUrl
+#' @importFrom httr GET write_disk
 #' @importFrom jsonlite fromJSON
-download_batch = function(batch_name, type= "ocr", odir=".") {
-    # TODO: change this to use httr, only support the ocr version?
-    if (type="data") {
-        url = paste0("https://chroniclingamerica.loc.gov/data/batches/", 
-		 batch_name)
-    }
-    if (type=="ocr") {
-        url = paste0("https://chroniclingamerica.loc.gov/data/ocr/", 
-		 batch_name, ".tar.bz2")
+download_batch_ocr_tar = function(batch_name, odir=".") {
+    fname = paste0(batch_name, ".tar.bz2")
+    url = paste0("https://chroniclingamerica.loc.gov/data/ocr/", fname)
 
-
-       # download into odir
-        content = RCurl::getURL(url)
-	if 404:
-	    redo but get the data
-    }
+    httr::GET(url, httr::write_disk(paste0(odir, "/", fname), overwrite=TRUE))
 }
 
 #' Load the text for a locally downloaded batch
 #'
-#' @param path character file path to a directory containing a batche's data
+#' @param path character file path to tar file for a batch
 #' @return dataframe of text_block, page, issue, date, year, month, day, lccn
-parse_batch = function(path) {
-    TODO:
+parse_batch_ocr_tar = function(path_to_tar) {
+    # eventually need to be able to handle already extracted tar files...
+    # for now extract to /tmp/ ...
+    #TODO:
     # Going to need to merge with the issues_df metadata at some point
+
+    # get file list
+    files = untar(path_to_tar, list=TRUE)
+
+    # extract
 }
